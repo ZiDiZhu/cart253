80b031f7fd72f10fa3987a25711962ec919d4778
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-10-18 19:50:24 -0400

    E4: pretty much completed

--- Exercises/exercise4/js/script.js
+++ Exercises/exercise4/js/script.js
@@ -7,40 +7,78 @@ Fun project with arrays
 "use strict";
 
 let arrayofGhosts = [];
-let numGhosts = 5;
+let numGhosts = 20;
 
 let ghostImage;
 let spGhostImage;
-let user;
+let user = {
+  x:0,
+  y:0,
+};
+let userImage;
+
+let life = 3;
+
+let state = 'game'; // goodend. badend
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ghostImage = loadImage('assets/images/ghost.png');
   spGhostImage = loadImage('assets/images/specialGhost.png');
+  userImage = loadImage('assets/images/user.png');
 
 //the number inside[] is called index, the content is called element
 
   for(let i =0; i<numGhosts; i++){
 
     //spawns a regular or special ghost by chance
-    let chance = random(0,10);
-    if(chance > 2){
-      let ghost = createGhost(random(50, width - 50), random(50, height - 50),'regular');
-      arrayofGhosts.push(ghost);
-    }else{
-      let ghost = createGhost(random(50, width - 50), random(50, height - 50),'special');
-      arrayofGhosts.push(ghost);
-    }
+    spawnGhost();
   }
 }
 
 function draw() {
   background(0);
 
-  for(let i =0; i<arrayofGhosts.length; i++){
-    moveGhost(arrayofGhosts[i]);
-    displayGhost(arrayofGhosts[i]);
+//game state
+  if(state ==='game'){
+
+    textAlign(LEFT);
+    fill(255);
+    text('press SPACE for more ghosts',20,20);
+    text('Life:'+ life,width/2,20);
+
+    for(let i =0; i<arrayofGhosts.length; i++){
+      moveGhost(arrayofGhosts[i]);
+      displayGhost(arrayofGhosts[i]);
+    }
+
+    user.x = mouseX;
+    user.y = mouseY;
+    imageMode("CENTER");
+    image(userImage,user.x,user.y);
+
+    checkCollision();
   }
+
+  if(life <1){
+    state = 'badend';
+  }
+
+//good end
+  if(state === 'goodend'){
+    textAlign(CENTER);
+    fill(255);
+    text(`this one loves you and will never ghost you \n refresh to replay`,width/2,height/2);
+    image(spGhostImage,width/2,height*2/3);
+  }
+
+  if(state === 'badend'){
+    textAlign(CENTER);
+    fill(255);
+    text('you feel sad from getting ghosted \n refresh to replay',width/2,height/2);
+    image(ghostImage,width/2,height*2/3);
+  }
+
 
 }
 
@@ -51,22 +89,48 @@ function createGhost(x,y,type){
     size: 50,
     vx:0,
     vy:0,
-    speed:2,
+    speed:3 ,
     type:type,
+    active:true
   };
   return ghost;
 }
 
+function spawnGhost(){
+  let chance = random(0,10);
+  if(chance > 2){
+    let ghost = createGhost(random(50, width - 50), random(50, height - 50),'regular');
+    arrayofGhosts.push(ghost);
+  }else{
+    let ghost = createGhost(random(50, width - 50), random(50, height - 50),'special');
+    arrayofGhosts.push(ghost);
+  }
+}
+
 function displayGhost(ghost){
-  push();
+
   imageMode(CENTER);
-  if(ghost.type === 'regular'){
+  if(ghost.type === 'regular'&& ghost.active === true){
     image(ghostImage,ghost.x,ghost.y);
   }
-  if(ghost.type === 'special'){
+  if(ghost.type === 'special'&& ghost.active === true){
     image(spGhostImage,ghost.x,ghost.y);
   }
-  pop();
+}
+
+function checkCollision(){
+
+  for(let i =0; i<arrayofGhosts.length; i++){
+    let d = dist(user.x, user.y,arrayofGhosts[i].x,arrayofGhosts[i].y);
+    if (d < 50 ){
+      if(arrayofGhosts[i].type === 'regular'&&arrayofGhosts[i].active === true){
+        arrayofGhosts[i].active = false;
+        life -=1;
+      }else if(arrayofGhosts[i].type === 'special'){
+        state = 'goodend';
+      }
+    }
+  }
 }
 
 function moveGhost(ghost){
@@ -86,19 +150,18 @@ function moveGhost(ghost){
   ghost.x = constrain(ghost.x, ghost.size, width - ghost.size);
   ghost.y = constrain(ghost.y, ghost.size, height - ghost.size);
 
+  if(ghost.type ==='regular'){
+
+  }
+
 }
 
 function keyPressed(){
 
   //press space to generate more ghosts
   if (keyCode === 32){
-    let chance = random(0,10);
-    if(chance > 2){
-      let ghost = createGhost(random(50, width - 50), random(50, height - 50),'regular');
-      arrayofGhosts.push(ghost);
-    }else{
-      let ghost = createGhost(random(50, width - 50), random(50, height - 50),'special');
-      arrayofGhosts.push(ghost);
+    if(state ==='game'){
+      spawnGhost();
     }
   }
 }
