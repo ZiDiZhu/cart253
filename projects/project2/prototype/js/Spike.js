79421181d7734b4801a1a19822fd9a669d47ab93
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-11-06 03:16:18 -0500

    P2: Fixed collision problem with spikes

--- projects/project2/prototype/js/Spike.js
+++ projects/project2/prototype/js/Spike.js
@@ -29,11 +29,14 @@ class Spike{
     //dist between circle edge and 3 verticles of the triangle
 
     for(let i = 0; i < player.length; i++){
-      let d1 = dist(player[i].x,player[i].y,this.x1,this.y1) - player[i].size/2;
-      let d2 = dist(player[i].x,player[i].y,this.x2,this.y2) - player[i].size/2;
-      let d3 = dist(player[i].x,player[i].y,this.x3,this.y3) - player[i].size/2;
+      let d1 = dist(player[1].x,player[1].y,this.x1,this.y1) - player[1].size/2;
+      let d2 = dist(player[1].x,player[1].y,this.x2,this.y2) - player[1].size/2;
+      let d3 = dist(player[1].x,player[1].y,this.x3,this.y3) - player[1].size/2;
+      let d4 = dist(player[0].x,player[0].y,this.x1,this.y1) - player[i].size/2;
+      let d5 = dist(player[0].x,player[0].y,this.x2,this.y2) - player[0].size/2;
+      let d6 = dist(player[0].x,player[0].y,this.x3,this.y3) - player[0].size/2;
 
-      if(d1>0 && d2>0 && d3>0){
+      if(d1>0 && d2>0 && d3>0 && d4>0 && d5>0 && d6>0){
         this.isTouchingPlayer = false;
       }else{
         this.isTouchingPlayer = true;
--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -19,8 +19,8 @@ function setup() {
 
   createCanvas (800,600);
 
-  player[0] = new Player(50,255);
-  player[1] = new Player(50,555);
+  player[0] = new Player(50,555);
+  player[1] = new Player(50,255);
 
   //test room platforms-dowm
   floor[0] = new Floor(330,20,0,580);
@@ -30,13 +30,14 @@ function setup() {
   floor[2] = new Floor(130,20,0,280);
   floor[3] = new Floor(400,20,200,280);
   floor[4] = new Floor(150,20,670,280);
+  floor[5] = new Floor(800,5,0,280);
 
   //test room spikes - down
   spike[0] = new Spike(330,600,410,600,370,550);
   spike[1] = new Spike(410,600,490,600,450,550);
 
   //test room spikes - up
-  spike[2] = new Spike(130,600,200,300,165,250);
+  spike[2] = new Spike(130,300,200,300,165,250);
   spike[3] = new Spike(600,300,670,300,635,250);
 }
 
